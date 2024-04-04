8441d0f147aa85c1eb542dc59852540ad8b61533
liyoway <liyoway@gmail.com>
2024-04-04 12:28:59 +0800

    UIKitとAVFoundationフレームワークをインポートしています。UIKitはiOSアプリのグラフィカルユーザーインターフェースを構築および管理するために使われ、AVFoundationはオーディオとビデオの再生を扱います。
        UIViewControllerを継承したViewControllerクラスを定義しています。これはiOSアプリ構造の標準で、アプリのビューコントローラーを管理するために使用されます。
    
        ViewController内にsakutaEmitterCellという関数を定義し、桜の花びらが舞い落ちるエフェクトを作成および設定しています。これはCAEmitterCellとCAEmitterLayerの設定を通じて実現され、桜の画像、発生率、寿命、速度、スケール、加速度、および放出範囲などを含みます。
    
        背景色と透明度を設定し、viewDidLoadメソッド内でsakutaEmitterCell関数を呼び出して桜の花びらが舞い落ちるエフェクトをトリガーしています。
    
        音楽再生のためにAVPlayerオブジェクトを作成しました。viewDidLoadメソッド内で“Spring – Sakura Girl (No Copyright Music)”というmp3音楽ファイルを読み込み、プレイアイテムを設定し、音量を調整して再生を開始しています。
    
    このコードの主な目的は、iOSアプリに視覚的および聴覚的な桜のテーマ体験を追加することで、桜の花びらが舞い落ちるエフェクトと関連するバックグラウンドミュージックによって春の雰囲気を作り出すことです。

--- gifPlaying/ViewController.swift
+++ gifPlaying/ViewController.swift
@@ -8,7 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func sakutaEmitterCell() -> UIView {
+        let sakutaEmitterCell = CAEmitterCell()
+        sakutaEmitterCell.contents = UIImage(named: "sakura_fubuki")?.cgImage
+        sakutaEmitterCell.birthRate = 3
+        sakutaEmitterCell.lifetime = 20
+        sakutaEmitterCell.velocity = 100
+        sakutaEmitterCell.scale = 0.5
+        sakutaEmitterCell.scaleRange = 0.3
+        sakutaEmitterCell.yAcceleration = 70
+        sakutaEmitterCell.emissionRange = CGFloat.pi
 
+        let sakuraEmitterLayer = CAEmitterLayer()
+        sakuraEmitterLayer.emitterCells = [sakutaEmitterCell]
+        sakuraEmitterLayer.emitterPosition = CGPoint(x: view.bounds.width / 2, y: -50)
+        sakuraEmitterLayer.emitterSize = CGSize(width: view.bounds.width, height: 0)
+        sakuraEmitterLayer.emitterShape = .cuboid
+        view.layer.addSublayer(sakuraEmitterLayer)
+    }
+    
+    let player = AVPlayer()
+
+    @IBOutlet weak var sakura2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
